79bd5ec1ea1a4680c25411a6f1ec45cab83e22d8
bluehope79 <kangel79@gmail.com>
2018-10-27 15:09:06 +0900

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,8 @@ var Board = require('../models/board');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  Board.find({}, function (err, board) {
-      res.render('index', { title: '찬송가봇', board: board });
+  Board.find({}.sort({ seq: 'ascending' }), function (err, board) {
+      res.render('index', { title: '찬송가목록', board: board });
   });
 });
 
